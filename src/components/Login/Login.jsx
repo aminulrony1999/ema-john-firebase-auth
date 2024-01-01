640b4b797c46bc41a37ad72d8949cdf94c1980d6
aminulrony1999 <aminulrony1999@gmail.com>
2024-01-02 02:22:31 +0530

    sign in function working properly

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 const Login = () => {
+  const [error,setError] = useState(null);
+  const {signIn} = useContext(AuthContext);
+  const handleSignIn = (event) =>{
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    setError('');
+    signIn(email,password)
+    .then(user => {
+      console.log('User Sign In successfull');
+    })
+    .catch(error =>{
+      setError(error.message);
+    })
+    form.reset();
+  }
   return (
     <div className="form-container">
       <h2 className="form-title">Login</h2>
-      <form>
+      <form onSubmit={handleSignIn}>
         <div className="form-control">
           <label htmlFor="email">Email</label>
           <input type="email" name="email" id="" required />
@@ -17,6 +35,7 @@ const Login = () => {
         <input className="btn-submit" type="submit" value="Login" />
       </form>
       <p><small>New to Ema John?<Link to="/signup">Create a new account</Link></small></p>
+      <p className="text-error">{error}</p>
     </div>
   );
 };
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from "../firebase/firebase.config";
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -8,8 +8,12 @@ const AuthProvider = ({ children }) => {
   const createNewUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
+  const signIn = (email,password) => {
+    return signInWithEmailAndPassword(auth,email,password);
+  }
   const authInfo = {
     createNewUser,
+    signIn
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
